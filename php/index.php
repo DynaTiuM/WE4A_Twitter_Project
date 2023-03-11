9a29039ce664172ceed3cf1e29a07b00b1d8d229
Raphael PERRIN <raphael.perrin754@gmail.com>
2023-03-11 20:34:22 +0100

    Beginning project

---
+++ php/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang =fr">
+
+<head>
+    <meta charset = "utf-8">
+    <title>Kitter</title>
+</head>
+<body>
+    <ul>
+        <li>Accueil</li>
+        <li>Explorer</li>
+        <li>Notifications</li>
+        <li>Messages</li>
+        <li>Profil</li>
+    </ul>
+</body>
+
+</html>
+
