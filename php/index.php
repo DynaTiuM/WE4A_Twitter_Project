2692a1f332df2ae2acf464a47076be8fc0caa57b
Raphael PERRIN <raphael.perrin754@gmail.com>
2023-03-14 20:56:55 +0100

    Adding first Google Map to a tweet

--- php/index.php
+++ php/index.php
@@ -7,6 +7,45 @@
     <link rel = "stylesheet" href = "./css/stylesheet.css">
     <title>Twitturtle</title>
     <link rel="shortcut icon" href="./favicon.ico">
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyATs7fPlZoEbjUiYV4dpOwg2rz4dOmtPuQ&callback=initMap"></script>
+
+    <script>
+        function initMap() {
+            if (document.getElementById('map') != null) {
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    center: {lat: 48.8534, lng: 2.3488}, // Coordonnées de Paris
+                    zoom: 13
+                });
+
+                var marker = new google.maps.Marker({
+                    position: map.getCenter(),
+                    map: map,
+                    draggable: true
+                });
+
+                google.maps.event.addListener(marker, 'dragend', function() {
+                    document.getElementById('latitude').value = marker.getPosition().lat();
+                    document.getElementById('longitude').value = marker.getPosition().lng();
+                });
+
+                google.maps.event.addListener(map, 'click', function(event) {
+                    marker.setPosition(event.latLng);
+                    document.getElementById('latitude').value = event.latLng.lat();
+                    document.getElementById('longitude').value = event.latLng.lng();
+                });
+            }
+        }
+
+        function openModal() {
+            document.getElementById('modal').style.display = 'block';
+            initMap();
+        }
+
+        function closeModal() {
+            document.getElementById('modal').style.display = 'none';
+        }
+    </script>
+
 </head>
 
 <body>
@@ -22,7 +61,7 @@
             <li class = "NavigationButton"><a href = "connect.php">Se connecter</a></li>
         </ul>
         <div class = "center">
-            <li class = "NavigationTweeter">Tweeter</li>
+            <li class = "NavigationTweeter">Message</li>
         </div>
     </div>
 
@@ -33,10 +72,31 @@
             <textarea class = "Content" placeholder="Quoi de neuf ?" rows="1" maxlength="240"></textarea>
             <span class = "Border" style="width: 80%;"></span>
             <div class = "ButtonPosition">
-                <a><button class = "Tweeter">Tweeter</button></a>
+                <a><button class = "Tweeter">Envoyer</button></a>
             </div>
+            <select>
+                <option value ="option1"></option>
+                <option value ="option2">PET 1</option>
+                <option value ="option3">PET 2</option>
+            </select>
             <p>HASHTAG</p>
-            <p>EMPLACEMENT</p>
+            <button onclick="openModal()">Choisir ma localisation</button>
+
+            <div id="modal">
+                <div id="modal-content">
+                    <span onclick="closeModal()" style="float:right">&times;</span>
+                    <h3>Choisir une localisation sur la carte</h3>
+                    <div id="map" style="height: 400px"></div>
+                    <form>
+                        <label for="latitude">Latitude :</label>
+                        <input type="text" id="latitude" name="latitude"><br>
+
+                        <label for="longitude">Longitude :</label>
+                        <input type="text" id="longitude" name="longitude"><br>
+                    </form>
+                </div>
+            </div>
+
         </div>
         <div class = "TimeLine">
             <p>TIMELINE</p>
